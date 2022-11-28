f8bce73ae9102e39009d8d98686ffa2fe9301987
C4EJS131 - Đoàn Tiến Triển <triendoan4432@gmail.com>
2022-11-29 02:51:51 +0700

    Json server

--- project/src/App.js
+++ project/src/App.js
@@ -1,14 +1,14 @@
 import HeaderWeb from "./components/header";
 import FooterPage from "./components/footer";
+import HomePage from "./pages/home/home";
 
-import LoginForm from "./components/loginform/loginform";
-import RegisterForm from "./components/registerform/registerform";
+// import LoginForm from "./components/loginform/loginform";
+// import RegisterForm from "./components/registerform/registerform";
 function App() {
   return (
     <div className="App">
-      
       <HeaderWeb />
-        <RegisterForm/>
+        <HomePage></HomePage>
       <FooterPage />
     </div>
   );
--- project/src/pages/home/home.js
+++ project/src/pages/home/home.js
@@ -1,7 +1,33 @@
+import {useState, useEffect} from "react"
+import {Link} from "react-router-dom"
+
 function HomePage() {
+    const [shoes, setShoes] = useState([]);
+
+    useEffect(() => {
+    fetch("http://localhost:3900/shoes")
+      .then((res) => res.json())
+      .then((data) => { 
+        setShoes(data);
+      });
+    }, []);
+
+
     return ( 
         <>
-            <h1>Home</h1>
+            {shoes.map((shoes) => (
+                <tr key={shoes.id}>
+                    <td>{shoes.name}</td>
+                    <td>{shoes.size}</td>
+                    <img src={shoes.images}></img>
+                    <td>{shoes.description}</td>
+                    <td>{shoes.price}</td>
+                    
+                    <Link to={`/products/${shoes.id}`}>Detail</Link>
+                </tr>
+            ))};
+
+                
         </>
      );
 }
