336984e0b8e8f289616271a1c018085be1380fd6
C4EJS131 - Đoàn Tiến Triển <triendoan4432@gmail.com>
2022-12-13 18:30:53 +0700

    Update

--- project/src/components/loginform/loginform.js
+++ project/src/components/loginform/loginform.js
@@ -1,45 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import axios from 'axios'
 import {useNavigate} from "react-router-dom"
 
 function LoginForm() {
-
   const navigate = useNavigate();
-  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState();
 
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
+    }
+  }, []);
 
-  const handleLogin= (e) =>{
+  // logout the user
+  const handleLogout = () => {
+    navigate('/');
+    setUser({});
+    setUsername("");
+    setPassword("");
+    localStorage.clear();
+  };
+
+  // login the user
+  const handleSubmit = async e => {
     e.preventDefault();
-    axios.get("http://localhost:3000/profile",{
-      user: user,
-      password : password,
-    }).then(result =>{
-      console.log(result);
-      
-        navigate("/");
-      
-    }).catch(error =>{
-      console.log(error);
-      alert("Đăng nhập thất bại");
-    })
+    const user = { username, password };
+    // send the username and password to the server
+    const response = await axios.get(
+      "http://localhost:3000/profile",
+      user
+    );
+    setUser(response.data);
+    localStorage.setItem("user", JSON.stringify(response.data));
+  };
+  if (user) {
+    return (
+      <div>
+        {user.name} is loggged in
+        <button onClick={handleLogout}>logout</button>
+      </div>
+    );
   }
+
+  
 
   return (
     <>
-      <form onSubmit={handleLogin}>
+      <form onSubmit={handleSubmit}>
       <div className={styles.form}>
       <h1 className={styles.h1}>Login Form</h1>
       <div className={styles.user}>
         <label className={styles.lable}>Username </label>
         <input
-          name="user"
+          name="username"
           type="text"
           placeholder="UserName"
           className={styles.input}
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         ></input>
       </div>
       <div className={styles.pass}>
@@ -54,7 +77,7 @@ function LoginForm() {
         ></input>
       </div>
       <div></div>
-      <button className={styles.button}>Login</button>
+      <button className={styles.button} typeof='submit'>Login</button>
     </div>
       </form>
     </>
